de6db256df90c83bc88662ab59deee9a92545cb6
Matthew Roesener <roesenerm@gmail.com>
2016-11-17 21:01:06 -0800

    added static assets

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var app = express();
 
+app.use("/", express.static("public"));
+
 app.get("/", function(req, res, next) {
 	res.sendFile(__dirname + "/public/index.html");
 });
