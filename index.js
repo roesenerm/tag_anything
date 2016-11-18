f354113cfbea496482f236b2e62f0f265125094b
Matthew Roesener <roesenerm@gmail.com>
2016-11-17 20:47:17 -0800

    Added basic express index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var app = express();
+
+app.get("/", function(req, res, next) {
+	res.send("Hello World!");
+});
+
+app.listen(3001, function() {
+	console.log("Rock on!");
+});
