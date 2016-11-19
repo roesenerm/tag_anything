6f5c21ec4ee984ed481a58a1c91541b12b9e28c2
Matthew Roesener <roesenerm@gmail.com>
2016-11-18 17:17:43 -0800

    Setup basic angular router

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 app.use("/", express.static("public"));
 app.use("/", express.static("bower_components"));
 
-app.get("/", function(req, res, next) {
+app.get("/*", function(req, res, next) {
 	res.sendFile(__dirname + "/public/index.html");
 });
 
--- public/js/app.js
+++ public/js/app.js
@@ -2,5 +2,23 @@
 
 (function(){
 	angular
-	.module("tagAnything", []);
+	.module("tagAnything", [
+		"ui.router"
+	])
+	.config(Router);
+
+	Router.$inject = ["$stateProvider", "$locationProvider", "$urlRouterProvider"];
+	function Router($stateProvider, $locationProvider, $urlRouterProvider){
+		$locationProvider.html5Mode(true);
+		$stateProvider
+		.state("main", {
+			url: "/",
+			template: "<h2>This is working!<h2>"
+		})
+		.state("test", {
+			url: "/test",
+			template: "<h2>This is also working!<h2>"
+		});
+		$urlRouterProvider.otherwise("/");
+	}
 })();
